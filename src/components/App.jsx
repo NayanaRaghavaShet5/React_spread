f6f610d31e8223084e2f3f4b15a73ddf897c7d20
NayanaRaghavaShet5 <51402931+NayanaRaghavaShet5@users.noreply.github.com>
2020-06-06 21:35:07 +0530

    We can add the spread operator to array and objeect
    Here

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [listData, setListData] = useState([]);
+
+  function handleText(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
+  function onAdd() {
+    setListData(preValue => {
+      return [...preValue, text];
+    });
+    setText("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleText} type="text" value={text} />
+        <button onClick={onAdd}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {listData.map(data => (
+            <li>{data}</li>
+          ))}
         </ul>
       </div>
     </div>
